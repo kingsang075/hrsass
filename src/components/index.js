a95a17e185371100fe8de8980882b4b574931bb8
Kingsang <1596387521@qq.com>
2022-05-05 23:11:05 +0800

    提交

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,14 @@
 // 负责所有全局自定义组件注册
 import PageTools from './Pagetools'
 import UploadExcel from './UploadExcel'
+import ImageUpload from './ImageUpload'
+import Print from 'vue-print-nb'
 export default {
   install(Vue) {
     // 组件注册
     Vue.component('PageTools', PageTools) // 注册工具栏组件
     Vue.component('UploadExcel', UploadExcel) // 注册导入excel组件
+    Vue.component('ImageUpload', ImageUpload) // 注册图片上传组件
+    Vue.use(Print) // 注册打印组件
   }
 }
